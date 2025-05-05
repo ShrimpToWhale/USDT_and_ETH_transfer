fd15713e7e3826632257d1ec3aa3ce80befc4dcc
zero_0x_zero <130780781+ShrimpToWhale@users.noreply.github.com>
2025-05-05 16:41:34 +0300

    Update constants.py

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,3 +1,6 @@
+# Network configuration
 arb_rpc_url = 'https://1rpc.io/arb'
 arb_explorer_url = 'https://arbiscan.io/'
+
+# Contract address
 USDT_CONTRACT_ADDRESS = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'
