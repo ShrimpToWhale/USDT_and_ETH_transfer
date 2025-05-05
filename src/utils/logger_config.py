5abdbbe712803742d771de752d950305b646ae97
zero_0x_zero <130780781+ShrimpToWhale@users.noreply.github.com>
2025-05-05 17:13:40 +0300

    Update logger_config.py

--- src/utils/logger_config.py
+++ src/utils/logger_config.py
@@ -2,12 +2,17 @@ from loguru import logger
 from datetime import datetime
 import sys
 
+# Generate a timestamp for the log file name.
 current_data_and_time = datetime.now().strftime('%H-%M_%d-%m-%y')
 
+# Define the log message format.
 log_format = '<green>{time:HH:mm:ss}</green> | <level>{level}</level> | <cyan>{module}:{line}</cyan> | <level>{message}</level>'
 
+# Remove the default Loguru handler.
 logger.remove()
 
+# Add file-based logger with timestamped filename.
 logger.add(sink=f'./user_data/logs/{current_data_and_time}.log', format=log_format, enqueue=True)
 
+# Add console logger for real-time output.
 logger.add(sink=sys.stdout, format=log_format, enqueue=True)
